f218e774e2594ac360c489e14cb00418a531c61b
LemonMantis5571 <briter456@gmail.com>
2023-11-20 23:28:28 -0600

    Fix GetTeamById

--- app/actions/getTeamById.ts
+++ app/actions/getTeamById.ts
@@ -24,6 +24,7 @@ export default async function getTeamById(
                         type: true,
                         type2: true,
                         moves: true,
+                        evs: true,
                     }
                 }
             }
